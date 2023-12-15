23a5b9ba48c995f4edfa3ae38eda00cb37edfc88
rogueassassin11 <maecagadas2011@gmail.com>
2023-12-15 22:12:55 +0800

    Did Header, About, and Portfolio Samples

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 // Add custom JavaScript here
+
+// Typing effect
+function runTypingEffect() {
+  const text = "I am Elias Ainsworth.";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
